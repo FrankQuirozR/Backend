08d53f166fb6ebc25cb396f7cb701c6293abf278
frank.quiroz.ramirez.082000@gmail.com <165614620+FrankQuirozR@users.noreply.github.com>
2024-11-13 20:51:03 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require("cors");
+const axios = require('axios');
+const app = express();
+
+// Usa la variable de entorno FRONTEND_URL
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000'
+}));
+
+// Usa la variable de entorno PORT
+const port = process.env.PORT || 3003;
+
+app.get('/webair', async (req, res) => {
+    try {
+        const response = await axios.get('http://api.canair.io:8080/dwc/stations');
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error al obtener datos de canair:', error);
+        res.status(500).json({ 
+            error: 'Error al obtener datos de las estaciones',
+            details: error.message 
+        });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
