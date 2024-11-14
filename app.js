b89e79b8319768afe5472c026f84312645ecbee7
frank.quiroz.ramirez.082000@gmail.com <165614620+FrankQuirozR@users.noreply.github.com>
2024-11-14 12:38:55 -0500

    edicion3

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const cors = require("cors");
 const axios = require('axios');
+const https = require('https');
 const app = express();
 
 // Load environment variables from .env file
 require('dotenv').config();
+
+// Disable SSL/TLS verification in Axios
+axios.defaults.httpsAgent = new https.Agent({  
+  rejectUnauthorized: false
+});
 
 // Use the FRONTEND_URL environment variable
 app.use(cors({
@@ -16,7 +22,7 @@ const port = process.env.PORT || 3003;
 
 app.get('/webair', async (req, res) => {
     try {
-        const response = await axios.get('http://api.canair.io:8080/dwc/stations');
+        const response = await axios.get('https://api.canair.io:8080/dwc/stations');
         res.json(response.data);
     } catch (error) {
         console.error('Error al obtener datos de canair:', error);
